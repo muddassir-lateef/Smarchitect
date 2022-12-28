1513ca39f9f6253640f5269d77fba765adfc59e3
Muddassir Lateef <muddassirlateef001@gmail.com>
2022-12-28 17:38:24 +0500

    Added midpoints for joining objects

--- frontend/src/components/JoinPainter.js
+++ frontend/src/components/JoinPainter.js
@@ -31,6 +31,7 @@ export const JoinPainter = (props) => {
     //    p3                    p4
 
     const CoordinateTranslator = (x, y, w, h, angle) => {
+
         var coords = []
 
         //p1
@@ -46,7 +47,21 @@ export const JoinPainter = (props) => {
 
         //p4
         coords.push([x3 + (w * Math.cos((angle) * (Math.PI / 180))), y3 + (w * Math.sin((angle) * (Math.PI / 180)))])
+
+        // midpoint of p1 and p3
+        coords.push([(coords[0][0] + coords[2][0]) / 2, (coords[0][1] + coords[2][1]) / 2])
+
+        // midpoint of p1 and p2
+        coords.push([(coords[0][0] + coords[1][0]) / 2, (coords[0][1] + coords[1][1]) / 2])
+
+        // midpoint of p3 and p4
+        coords.push([(coords[2][0]+coords[3][0])/2,(coords[2][1]+coords[3][1])/2])
+
+        // midpoint of p2 and p4
+        coords.push([(coords[1][0]+coords[3][0])/2,(coords[1][1]+coords[3][1])/2])
+
         return coords
+
 
     };
     const makeJoins = (xx, yy, id1, id2) => {
@@ -107,8 +122,8 @@ export const JoinPainter = (props) => {
         for (var img = 0; img < ImageObjects.length; img++) {
             if (ImageObjects[img] != null) {
                 var coords1 = CoordinateTranslator(ImageObjects[img].x, ImageObjects[img].y, ImageObjects[img].width, ImageObjects[img].height, ImageObjects[img].rotation)
-                for (var i = 0; i < 4; i++) {
-                    for (var j = 0; j < 4; j++) {
+                for (var i = 0; i < coords2.length; i++) {
+                    for (var j = 0; j < coords2.length; j++) {
 
                         if (checkJoins(coords1[i], coords2[j])) {
                             if (checkIds(dbContext.selectedImgInstance, ImageObjects[img].id, coords1[i])) {
@@ -120,12 +135,46 @@ export const JoinPainter = (props) => {
             }
         }
 
+
+
         // Checking to Remove all unattached joins
         var toRemove = []
+
         for (var i = 0; i < Joins.length; i++) {
 
             for (var x = 0; x < Joins.length; x++) {
-                
+
+            }
+            if (Joins[i].img1Id == dbContext.selectedImgInstance || Joins[i].img2Id == dbContext.selectedImgInstance) {
+                var valid = false;
+                for (var j = 0; j < coords2.length; j++) {
+                    if (checkJoins([Joins[i].x, Joins[i].y], coords2[j])) {
+
+                        valid = true
+                    }
+                }
+                if (!valid) {
+                    toRemove.push(i)
+                }
+            }
+
+        }
+
+        //Remove all unattached joins
+        for (var i = toRemove.length - 1; i >= 0; i--) {
+
+            Joins.splice(toRemove[i], 1)
+        }
+
+
+
+        // making double joins into singles
+        toRemove = []
+
+        for (var i = 0; i < Joins.length; i++) {
+
+            for (var x = 0; x < Joins.length; x++) {
+
             }
             if (Joins[i].img1Id == dbContext.selectedImgInstance || Joins[i].img2Id == dbContext.selectedImgInstance) {
                 var valid = false;
@@ -142,11 +191,7 @@ export const JoinPainter = (props) => {
 
         }
 
-        //Remove all unattached joins
-        for (var i = toRemove.length-1; i >= 0; i--) {
 
-            Joins.splice(toRemove[i], 1)
-        }
     }, [ImageChanged])
 
     return (
